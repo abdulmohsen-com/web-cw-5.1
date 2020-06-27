12629a3f5c2ebd315f60b224628d61c2e9994b98
abdulmohsen-com <66735197+abdulmohsen-com@users.noreply.github.com>
2020-06-27 20:04:19 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,37 @@
 // السؤال الأول
 // 1
-
+let name = ("عبدالمحسن");
 // 2
-
+let age = ("14") ;
 // 3 and 4
-
+console.log("انا" + " " + name + " " + "وعمري" + " " + age);
 // 5 and 6
-
+age *= 5;
+console.log(age);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobby =['art, reading'];
 // 2
-
+hobby.push(14);
 // 3
-
+hobby.pop();
+console.log(hobby);
 // 4
-
+let chioce ={
+    language: "JavaScript"
+};
 // 5
-
+let ta =["Moudhi", "Hussien", "Rahaf", "Nancy"];
 // 6
-
+ chioce = {
+    language: "JavaScript",
+    name: "web",
+    ta:["Moudhi", "Hussien", "Rahaf", "Nancy"]
+};
 // 7
-
+console.log(ta[1]);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
